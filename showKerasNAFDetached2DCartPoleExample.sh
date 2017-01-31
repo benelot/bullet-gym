c0e48cf4a86d67dcffb885b8e4b1054f4e0ac621
Benelot <be.ellenberger@gmail.com>
2017-01-31 21:04:42 +0100

    Delay test runs to make it easier to look at.

--- showKerasNAFDetached2DCartPoleExample.sh
+++ showKerasNAFDetached2DCartPoleExample.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-python runTrainer.py --agent=KerasNAFAgent --env=Detached2DCartPolev0Env --train-for=0 --test-for=10000000 --gui --show-test --load-file=KerasNAF-D2DCartPolev0-chkpt-1.h5
+python runTrainer.py --agent=KerasNAFAgent --env=Detached2DCartPolev0Env --train-for=0 --test-for=10000000 --delay=0.005 --gui --show-test --load-file=KerasNAF-D2DCartPolev0-chkpt-1.h5
 
