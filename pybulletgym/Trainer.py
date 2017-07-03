5c35d83292c4183068e4163e2667166018c9ad4e
Benelot <be.ellenberger@gmail.com>
2017-07-03 13:29:37 +0200

    Enable --continue-training.

--- pybulletgym/Trainer.py
+++ pybulletgym/Trainer.py
@@ -6,6 +6,7 @@ import time
 import gym
 from pybulletgym.agents import agent_register
 import pybullet as p
+import kerasrl_utils
 
 np.set_printoptions(precision=3, suppress=True, linewidth=10000)
 
@@ -41,23 +42,31 @@ class Trainer:
 			env = gym.make(opts.env)
 			
 			if 'discrete_actions' in agent.metadata or "continuous_actions" in env.metadata:
-					print("No idea if agent/environment combination is compatible. We will see.")
+				print("No idea if agent/environment combination is compatible. We will see.")
 			else:
-					if agent.metadata['discrete_actions'] and agent.metadata['discrete_actions'] != env.metadata['discrete_actions'] or not agent.metadata['discrete_actions'] and agent.metadata['discrete_actions'] == env.metadata['continuous_actions']:
-							print("Incompatible agent/environment pair!")
-							exit()
+				if agent.metadata['discrete_actions'] and agent.metadata['discrete_actions'] != env.metadata['discrete_actions'] or not agent.metadata['discrete_actions'] and agent.metadata['discrete_actions'] == env.metadata['continuous_actions']:
+					print("Incompatible agent/environment pair!")
+					exit()
 			
 			# configurations
 			env.seed(int(time.time()))
 			#env.configureActions(agent.metadata['discrete_actions']) # configure environment to accepts discrete actions
 			if agent.metadata['discrete_actions']:
-					agent.configure(env.observation_space.shape, env.action_space.n) # configure agent to use the environment properties
+				agent.configure(env.observation_space.shape, env.action_space.n) # configure agent to use the environment properties
 			else:
-					agent.configure(env.observation_space.shape, env.action_space.shape[0]) # configure agent to use the environment properties
-			
+				agent.configure(env.observation_space.shape, env.action_space.shape[0]) # configure agent to use the environment properties
+
+			if opts.continue_training:
+				properties = kerasrl_utils.get_latest_save("checkpoints/", opts.agent, opts.env, 0)
+				if properties == []:
+					print("No previous weight saves for found for %s-%s" % (opts.agent, opts.env))
+				else:
+					opts.load_file = "checkpoints/%s-%s-%s.h5" % (properties[0], properties[1], properties[2])
+					print("Continue training from: %s " % opts.load_file)
+
 			if opts.load_file is not None:
-					print("loading weights from [%s]" % opts.load_file)
-					agent.load_weights(opts.load_file)
+				print("loading weights from [%s]" % opts.load_file)
+				agent.load_weights(opts.load_file)
 			
 			# Okay, now it's time to learn something! We visualize the training here for show, but this
 			# slows down training quite a lot. You can always safely abort the training prematurely using
@@ -66,8 +75,8 @@ class Trainer:
 			
 			# After training is done, we save the final weights.
 			if opts.save_file is not None:
-					print("saving weights to [%s]" % opts.save_file)
-					agent.save_weights(opts.save_file, overwrite=True)
+				print("saving weights to [%s]" % opts.save_file)
+				agent.save_weights(opts.save_file, overwrite=True)
 
 			# Finally, evaluate our algorithm.
 			agent.test(env, nb_episodes=opts.test_for, visualize=self.visualize)
