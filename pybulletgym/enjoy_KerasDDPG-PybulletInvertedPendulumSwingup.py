7947c20e3d19067749278158284c13d77d6c2258
Benjamin Ellenberger <benjamin.ellenberger@intrasoft.ch>
2017-07-04 13:45:00 +0200

    Implement reacher.

---
+++ pybulletgym/enjoy_KerasDDPG-PybulletInvertedPendulumSwingup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import Trainer
+import datetime
+import argparse
+import pybulletgym.envs
+
+trainer = Trainer.Trainer()
+
+argparser = argparse.ArgumentParser()
+Trainer.add_opts(argparser)
+
+# precoded options
+opts = argparser.parse_args()
+opts.agent = "KerasDDPGAgent-v0"
+opts.env = "PybulletInvertedPendulumSwingup-v0"
+opts.train_for = 0
+opts.test_for = 10000000
+opts.delay=0.005
+opts.use_latest = True
+
+print("\n OPTS", opts)
+
+trainer.setup_exercise(opts)
+
